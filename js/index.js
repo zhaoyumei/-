34f2676ce84e1a0d496aef4f03327d476d9e1af7
zhaoyumei <zym5330@126.com>
2017-09-18 12:01:30 +0800

    2

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@
 ;(function(){
 	var oBannerBox = document.querySelector('.bannerBox');
 	var bannerList =  document.querySelector('.bannerList');
-	
+	var dotWrapper = document.querySelector('.bannerPoint');
+	var dots ;
 	var loop = true;
 	var pageIndex_G = 0;/*哪张图*/
 	var timer;/*定时器*/
@@ -18,8 +19,10 @@
 			li_arr.push('<img src="'+ item.img_src +'">');
 			li_arr.push('</a>');
 			li_arr.push('</li>');
+			dotWrapper.appendChild(document.createElement('li'));
 		}
 		bannerList.innerHTML = li_arr.join('');
+        dots = dotWrapper.querySelectorAll('li');
 	}
 	
 	/*初始化BScroll*/
@@ -41,23 +44,24 @@
 			if(loop) {
 				pageIndex -= 1
 			}
-			pageIndex_G = pageIndex
-			_play(slider)
+			pageIndex_G = pageIndex;
+			_initDots(pageIndex_G);
+			_play(slider);
 		});
 
 		slider.on('beforeScrollStart', function () {
-			clearTimeout(timer)
-		})
+			clearTimeout(timer);
+		});
 
 	}
 
 	function _setSliderWidth(isResize) {
 		var sliderWidth = oBannerBox.clientWidth;
 		var width = 0;
-		var childern =bannerList.querySelectorAll('li');
+		var children =bannerList.querySelectorAll('li');
 
-		for(var i = 0; i < childern.length; i++) {
-			var child = childern[i];
+		for(var i = 0; i < children.length; i++) {
+			var child = children[i];
 			child.style.width = sliderWidth + 'px';
 			width += sliderWidth;
 		}
@@ -69,19 +73,27 @@
 
 	function _play(slider) {
 		var pageIndex = pageIndex_G + 1;
-		pageIndex += 1 
+		pageIndex += 1;
 		timer = setTimeout(function () {
-			slider.goToPage(pageIndex, 0, 400)
+			slider.goToPage(pageIndex, 0, 400);
 		},1000)
+	}
+
+	function _initDots(pageIndex) {
+		for(var i = 0; i < dots.length; i++) {
+			dots[i].className = '';
+		}
+		dots[pageIndex].className = 'on';
+
 	}
 
 	function _getBannerData() {
 		$.ajax({
 			url: 'data.json',
 			success: function (res) {
-				// var res = $.parseJSON(res);
-				if (res.code == 0) {
+				if (res.code === 0) {
 					_createLi(res.list);
+
 				}
                 _setSliderWidth();
                 _initSlider();
@@ -95,5 +107,24 @@
 		_setSliderWidth(true);
 		slider.refresh();
 	});
+
 	_getBannerData();
+})()
+
+;(function () {
+
+	var scrollWrapper = document.querySelector('.mainBox');
+	var scroll;
+
+	function _initScroll() {
+		scroll = new BScroll(scrollWrapper,{
+			probeType: 1,
+			click: true
+		});
+	}
+	window.addEventListener('resize',function () {
+		scroll.refresh();
+	});
+
+	_initScroll();
 })();
